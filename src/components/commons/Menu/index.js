1b1c6a4ae72c45653f3e88c3b5a9db64a6b8a83b
mayrazan <mayramanchein@yahoo.com.br>
2021-07-29 19:53:43 -0300

    Finish day 5

--- src/components/commons/Menu/index.js
+++ src/components/commons/Menu/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Logo } from "../../../theme/Logo";
+import { Text } from "../../foundation/Text";
 import { Button } from "../Button";
 import { MenuWrapper } from "./styles/MenuWrapper";
 
@@ -20,7 +21,9 @@ export default function Menu() {
           { url: "/sobre", name: "Sobre" },
         ].map((link) => (
           <li key={link.url}>
-            <a href={link.url}>{link.name}</a>
+            <Text variant="smallestException" tag="a" href={link.url}>
+              {link.name}
+            </Text>
           </li>
         ))}
       </MenuWrapper.CentralSide>
