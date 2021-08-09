784f5ca399179abf1dcc0cc945e94c0ab122f7a0
mayrazan <mayramanchein@yahoo.com.br>
2021-08-09 13:25:51 -0300

    Finish day 2 - module 2

--- src/components/commons/Modal/index.js
+++ src/components/commons/Modal/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styled, { css } from 'styled-components';
+import { motion } from 'framer-motion';
 
 const ModalWrapper = styled.div`
   display: flex;
   flex-direction: column;
   align-items: stretch;
-  background: rgba(0, 0, 0, 0.1);
+  background: rgba(0, 0, 0, 0.9);
   position: fixed;
   top: 0;
   left: 0;
@@ -14,6 +15,8 @@ const ModalWrapper = styled.div`
   bottom: 0;
   margin: auto;
   overflow: scroll;
+  transition: 0.3s;
+  z-index: 100;
   ${({ isOpen }) => {
     if (isOpen) {
       return css`
@@ -27,32 +30,42 @@ const ModalWrapper = styled.div`
     `;
   }}
 `;
-
 function Modal({ isOpen, onClose, children }) {
   return (
     <ModalWrapper
       isOpen={isOpen}
       onClick={(event) => {
-        const isSafeArea = event.target.closest(
-          '[data-modal-safe-area="true"]',
-        );
-        // isOpen = false;
+        const isSafeArea = event.target.closest('[data-modal-safe-area="true"');
         if (!isSafeArea) {
           onClose();
         }
       }}
     >
-      {children({
-        'data-modal-safe-area': 'true',
-      })}
+      <motion.div
+        variants={{
+          open: {
+            x: 0,
+          },
+          closed: {
+            x: '-100%',
+          },
+        }}
+        animate={isOpen ? 'open' : 'closed'}
+        transition={{
+          duration: 0.5,
+        }}
+        style={{ display: 'flex', flex: 1 }}
+      >
+        {children({
+          'data-modal-safe-area': 'true',
+        })}
+      </motion.div>
     </ModalWrapper>
   );
 }
-
 Modal.propTypes = {
   isOpen: PropTypes.bool.isRequired,
   children: PropTypes.func.isRequired,
   onClose: PropTypes.func.isRequired,
 };
-
 export default Modal;
