ab69a97da022d52e745087b976703ae9db7c0865
mayrazan <mayramanchein@yahoo.com.br>
2021-09-06 16:33:14 -0300

    test(login): refactoring login tests

--- cypress/integration/pages/app/login.spec.js
+++ cypress/integration/pages/app/login.spec.js
@@ -1,34 +1,29 @@
 /// <reference types="cypress" />
+import LoginScreenPageObject from '../../../../src/components/screens/app/LoginScreen/LoginScreen.pageObject';
 
 describe('/pages/app/login/', () => {
-  // it === test que estamos fazendo
-  it('preencha os campos e vá para a página /app/profile', () => {
-    cy.intercept(
-      'https://instalura-api-git-master-omariosouto.vercel.app/api/login',
-    ).as('userLogin');
-    cy.visit('/app/login/');
+  describe('when fill and submit a form login request', () => {
+    it('go to the profile page', () => {
+      // Pré Teste
+      cy.intercept(
+        'https://instalura-api-git-master-omariosouto.vercel.app/api/login',
+      ).as('userLogin');
 
-    // preencher o input usuario
-    // document.querySelector('input[name="usuario"]')
-    cy.get('#formCadastro input[name="usuario"]').type('mayrazan');
+      // Cenário
+      const loginScreen = new LoginScreenPageObject(cy);
+      loginScreen
+        .fillLoginForm({ user: 'mayrazan', password: 'senhasegura' })
+        .submitLoginForm();
 
-    // preencher o input senha
-    cy.get('#formCadastro input[name="senha"]').type('senhasegura');
+      // Asserções
+      cy.url().should('include', '/app/profile');
 
-    // clicar no botão de submit!
-    cy.get('#formCadastro button[type="submit"]').click();
-
-    // o que esperamos? ir para "/app/profile/"
-    cy.url().should('include', '/app/profile');
-
-    cy.wait('@userLogin').then((intercept) => {
-      // token do servidor
-      const { token } = intercept.response.body.data;
-
-      cy.getCookie('APP_TOKEN')
-        .should('exist')
-        // token do cookie é igual ao do server?
-        .should('have.property', 'value', token);
+      cy.wait('@userLogin').then((intercept) => {
+        const { token } = intercept.response.body.data;
+        cy.getCookie('APP_TOKEN')
+          .should('exist')
+          .should('have.property', 'value', token);
+      });
     });
   });
 });
--- src/services/login/loginService.js
+++ src/services/login/loginService.js
@@ -1,4 +1,5 @@
 import { setCookie, destroyCookie } from 'nookies';
+import { isStagingEnv } from '../../infra/env/isStagingEnv';
 
 async function HttpClient(url, { headers, body, ...options }) {
   return fetch(url, {
@@ -17,18 +18,19 @@ async function HttpClient(url, { headers, body, ...options }) {
   });
 }
 
+const BASE_URL = isStagingEnv
+  ? 'https://instalura-api-git-master-omariosouto.vercel.app'
+  : 'https://instalura-api.omariosouto.vercel.app';
+
 export const loginService = {
   async login({ username, password }) {
-    return HttpClient(
-      'https://instalura-api-git-master-omariosouto.vercel.app/api/login',
-      {
-        method: 'POST',
-        body: {
-          username, // 'omariosouto'
-          password, // 'senhasegura'
-        },
+    return HttpClient(`${BASE_URL}/api/login`, {
+      method: 'POST',
+      body: {
+        username,
+        password,
       },
-    ).then((respostaConvertida) => {
+    }).then((respostaConvertida) => {
       const { token } = respostaConvertida.data;
       const DAY_IN_SECONDS = 86400;
       // Salvar o Token
